8082658bd4dfd2d252b12118715cbbe0f2c837f3
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 20:10:42 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -36,6 +36,34 @@ def _max_width_():
 
 # %%
 
+def get_topic_model(lines):
+    topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
+    topics, probs = topic_model.fit_transform(lines)
+    freq = topic_model.get_topic_info(); freq.head(5)
+    return topics, freq
+
+# %%
+   
+def topic_model_visualize(topic_model):
+    return topic_model.visualize_topics()
+
+# %%
+
+def topic_model_distribution(topic_model):
+    return topic_model.visualize_distribution(probs[200], min_probability=0.015)
+
+# %%
+
+def topic_model_hierarchy(topic_model):
+    return topic_model.visualize_hierarchy(top_n_topics=50)
+
+# %%
+
+def topic_model_barchart(topic_model):
+    return topic_model.visualize_barchart(top_n_topics=5)
+    
+# %%
+
 filename = None
 uploaded_file = st.sidebar.file_uploader('Carica un file .txt')
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
@@ -43,26 +71,9 @@ st.sidebar.markdown("""---""")
 if filename is not None:
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()    
-    
-    def get_topic_model(lines):
-        topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
-        topics, probs = topic_model.fit_transform(lines)
-        freq = topic_model.get_topic_info(); freq.head(5)
-        return topics, freq
-    
-        topic_model, topics = get_topic_model(lines)
+# %%
 
-        def topic_model_visualize(topic_model):
-            return topic_model.visualize_topics()
-
-        def topic_model_distribution(topic_model):
-            return topic_model.visualize_distribution(probs[200], min_probability=0.015)
-
-        def topic_model_hierarchy(topic_model):
-            return topic_model.visualize_hierarchy(top_n_topics=50)  
-
-        def topic_model_barchart(topic_model):
-            return topic_model.visualize_barchart(top_n_topics=5)
+topic_model, topics = get_topic_model(lines)
     
 fig1 = topic_model_visualize(topic_model)
 st.write(fig1)
