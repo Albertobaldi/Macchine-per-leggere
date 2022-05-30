ab74ea6738e2662f3bddcfe4a38ac22244e6eaa9
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 20:35:57 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -38,14 +38,15 @@ st.sidebar.markdown("""---""")
 if filename is not None:
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
+file = lines
     
-def get_topic_model(lines):
+def get_topic_model(file):
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
-    topics, probs = topic_model.fit_transform(lines)
+    topics, probs = topic_model.fit_transform(file)
     freq = topic_model.get_topic_info(); freq.head(5)
     return topics, freq, topic_model
 
-text, dates, topic_model, topics = get_topic_model(lines)
+text, dates, topic_model, topics = get_topic_model(file)
    
 def topic_model_visualize(topic_model):
     return topic_model.visualize_topics()
