1d4b3a450d0d7d92068a819a5da33d4a3c61535a
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 16:55:45 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -48,37 +48,37 @@ def BERTopic():
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True, vectorizer_model=vectorizer_model)
 
 # %%
-def topic_model.fit_transform():
+def topic_model_transform():
    topics, probs = topic_model.fit_transform(uploaded_file)
 
 # %%
-def topic_model.get_topic_info():
+def topic_model_get_topic_info():
    freq = topic_model.get_topic_info(); freq.head(5)
 
 # %%
-def topic_model.get_topic():
-   topic_model.get_topic(0)  # Select the most frequent topic
+def topic_model_get_topic():
+   return topic_model.get_topic(0)  # Select the most frequent topic
 
 # %%
-def topic_model.visualize_topics():
-   topic_model.visualize_topics()
+def topic_model_visualize_topics():
+   return topic_model.visualize_topics()
 
 # %%
-def topic_model.visualize_distribution():
-   topic_model.visualize_distribution(probs[200], min_probability=0.015)
+def topic_model_visualize_distribution():
+   return topic_model.visualize_distribution(probs[200], min_probability=0.015)
 
 # %%
-def topic_model.visualize_hierarchy():
-   topic_model.visualize_hierarchy(top_n_topics=50)
+def topic_model_visualize_hierarchy():
+   return topic_model.visualize_hierarchy(top_n_topics=50)
 
 # %%
-def topic_model.visualize_barchart():
-   topic_model.visualize_barchart(top_n_topics=5)
+def topic_model_visualize_barchart():
+   return topic_model.visualize_barchart(top_n_topics=5)
 
 # %%
-def topic_model.visualize_heatmap():
-   topic_model.visualize_heatmap(n_clusters=20, width=1000, height=1000)
+def topic_model_visualize_heatmap():
+   return topic_model.visualize_heatmap(n_clusters=20, width=1000, height=1000)
 
 # %&
-def topic_model.visualize_term_rank():
-   topic_model.visualize_term_rank()
+def topic_model_visualize_term_rank():
+   return topic_model.visualize_term_rank()
