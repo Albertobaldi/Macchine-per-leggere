bf429c06b7c8021700522952589c201801a6066c
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 18:14:52 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -31,8 +31,50 @@ def _max_width_():
     )
     
 # %%
+
+def get_topic_model(text):
+    topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
+    topics, probs = topic_model.fit_transform(text)
+    freq = topic_model.get_topic_info(); freq.head(5)
+    return text, topics, freq
+
+# %%
+
+def topic_model_visualize(topic_model)
+    return topic_model.visualize_topics()
+
+# %%
+
+def topic_model_distribution(topic_model)
+    return topic_model.visualize_distribution(probs[200], min_probability=0.015)
+
+# %%
+
+def topic_model_hierarchy(topic_model)
+    return topic_model.visualize_hierarchy(top_n_topics=50)
+    
+# %%
+
+def topic_model_barchart(topic_model)
+    return topic_model.visualize_barchart(top_n_topics=5)
+
+# %%
+
+# %%
 uploaded_file = st.sidebar.file_uploader('Carica un file .txt')
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
 st.sidebar.markdown("""---""")
 if uploaded_file is not None:
     text = pd.read_table(uploaded_file,header=None)
+    
+    fig1 = topic_model_visualize(topic_model)
+    st.write(fig1)
+    
+    fig2 = topic_model_distribution(topic_model)
+    st.write(fig2)
+    
+    fig3 = topic_model_hierarchy(topic_model)
+    st.write(fig3)
+    
+    fig4 = topic_model_barchart(topic_model)
+    st.write(fig4)
