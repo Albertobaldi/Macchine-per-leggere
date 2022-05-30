df08499da2adeef3dde748b40e51378357ce303d
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 17:12:42 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -36,9 +36,41 @@ stopwords = stopwords.words("italian")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     dataframe = pd.read_table(uploaded_file,header=None)
-    st.write(dataframe)
 
+# %%
 
+def preprocess(text_col):
+    """This function will apply NLP preprocessing lambda functions over a pandas series such as df['text'].
+       These functions include converting text to lowercase, removing emojis, expanding contractions, removing punctuation,
+       removing numbers, removing stopwords, lemmatization, etc."""
+    
+    # convert to lowercase
+    text_col = text_col.apply(lambda x: ' '.join([w.lower() for w in x.split()]))
+    
+    # remove emojis
+    text_col = text_col.apply(lambda x: demoji.replace(x, ""))
+    
+    # expand contractions  
+    text_col = text_col.apply(lambda x: ' '.join([contractions.fix(word) for word in x.split()]))
+
+    # remove punctuation
+    text_col = text_col.apply(lambda x: ''.join([i for i in x if i not in string.punctuation]))
+    
+    # remove numbers
+    text_col = text_col.apply(lambda x: ' '.join(re.sub("[^a-zA-Z]+", " ", x).split()))
+
+    # remove stopwords
+    stopwords = [sw for sw in list(nltk.corpus.stopwords.words('english')) + ['thing'] if sw not in ['not']]
+    text_col = text_col.apply(lambda x: ' '.join([w for w in x.split() if w not in stopwords]))
+
+    # lemmatization
+    text_col = text_col.apply(lambda x: ' '.join([WordNetLemmatizer().lemmatize(w) for w in x.split()]))
+
+    # remove short words
+    text_col = text_col.apply(lambda x: ' '.join([w.strip() for w in x.split() if len(w.strip()) >= 3]))
+
+    return text_col
+    
 # %%
 from sklearn.feature_extraction.text import CountVectorizer
 
