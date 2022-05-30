f23c5b05fc4785ff0ef73d88308ea542bb86866c
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 18:31:48 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -66,7 +66,7 @@ uploaded_file = st.sidebar.file_uploader('Carica un file .txt')
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
 st.sidebar.markdown("""---""")
 if uploaded_file is not None:
-    df = pd.read_table(uploaded_file,header=None)
+    df = pd.read_table(uploaded_file,header=None).split('\n')
     cleaned_df = df.astype(str)
     text, topic_model, topics = get_topic_model(cleaned_df)
     
