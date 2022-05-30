1ee324c2e8f9496992238a96f4dfc8b394df09ef
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-30 16:25:57 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -10,7 +10,7 @@ stopwords = stopwords.words(italian)
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     string_data = stringio.read()
-     st.write(string_data)
+    st.write(string_data)
 
 
 # %%
