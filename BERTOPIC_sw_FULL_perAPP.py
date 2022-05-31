bd4d1fe15e45d0f1ea0223b9c7ea38562521e824
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-31 15:00:57 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -52,6 +52,6 @@ uploaded_file = st.sidebar.file_uploader("Scegli un file di testo")
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
 st.sidebar.markdown("""---""")
 if uploaded_file is not None:
-    open(uploaded_file) as f:
+    f = open(uploaded_file)
     file = f.readline(uploaded_file)
     get_topic_model(file)
