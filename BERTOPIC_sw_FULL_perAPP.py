f2f067b015bbea4db1bc192ff0987314236b03d4
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-31 11:32:03 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -45,36 +45,36 @@ if df is not None:
     with open(df, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         def get_topic_model(lines):
-        topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
-        topics, probs = topic_model.fit_transform(lines)
-        freq = topic_model.get_topic_info(); freq.head(5)
-        return topics, freq, topic_model
+            topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
+            topics, probs = topic_model.fit_transform(lines)
+            freq = topic_model.get_topic_info(); freq.head(5)
+            return topics, freq, topic_model
     
-        def topic_model_visualize(topic_model):
-        return topic_model.visualize_topics()
+            def topic_model_visualize(topic_model):
+            return topic_model.visualize_topics()
 
-        def topic_model_distribution(topic_model):
-        return topic_model.visualize_distribution(probs[200], min_probability=0.015)
+            def topic_model_distribution(topic_model):
+            return topic_model.visualize_distribution(probs[200], min_probability=0.015)
 
-        def topic_model_hierarchy(topic_model):
-        return topic_model.visualize_hierarchy(top_n_topics=50)
+            def topic_model_hierarchy(topic_model):
+            return topic_model.visualize_hierarchy(top_n_topics=50)
 
-        def topic_model_barchart(topic_model):
-        return topic_model.visualize_barchart(top_n_topics=5)
+            def topic_model_barchart(topic_model):
+            return topic_model.visualize_barchart(top_n_topics=5)
         
-        if st.button('Esegui l’analisi'):
-        tm_state = st.text('Modeling topics...')
-        text, dates, topic_model, topics = get_topic_model(lines)
-        tm_state.text('Modeling topics... done!')
+            if st.button('Esegui l’analisi'):
+            tm_state = st.text('Modeling topics...')
+            text, dates, topic_model, topics = get_topic_model(lines)
+            tm_state.text('Modeling topics... done!')
     
-        with st.container():
-        st.write("This is inside the container")
+            with st.container():
+            st.write("This is inside the container")
 
-        fig1 = topic_model_visualize(topic_model)
-        st.write(fig1)
-        fig2 = topic_model_distribution(topic_model)
-        st.write(fig2)
-        fig3 = topic_model_hierarchy(topic_model)
-        st.write(fig3)
-        fig4 = topic_model_barchart(topic_model)
-        st.write(fig4)
+            fig1 = topic_model_visualize(topic_model)
+            st.write(fig1)
+            fig2 = topic_model_distribution(topic_model)
+            st.write(fig2)
+            fig3 = topic_model_hierarchy(topic_model)
+            st.write(fig3)
+            fig4 = topic_model_barchart(topic_model)
+            st.write(fig4)
