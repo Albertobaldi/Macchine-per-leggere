f78541cd7eaf34398560c1ccb5b08c7808a0687a
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-31 11:31:17 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -37,38 +37,37 @@ def _max_width_():
         unsafe_allow_html=True,
     )
     
-def get_topic_model(lines):
-    topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
-    topics, probs = topic_model.fit_transform(lines)
-    freq = topic_model.get_topic_info(); freq.head(5)
-    return topics, freq, topic_model
-    
-def topic_model_visualize(topic_model):
-    return topic_model.visualize_topics()
-
-def topic_model_distribution(topic_model):
-    return topic_model.visualize_distribution(probs[200], min_probability=0.015)
-
-def topic_model_hierarchy(topic_model):
-    return topic_model.visualize_hierarchy(top_n_topics=50)
-
-def topic_model_barchart(topic_model):
-    return topic_model.visualize_barchart(top_n_topics=5)
-    
 df = None
 uploaded_file = st.sidebar.file_uploader('Carica un file .txt')
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
 st.sidebar.markdown("""---""")
 if df is not None:
     with open(df, 'r', encoding='utf-8') as f:
-        lines = f.readlines()    
-        
-if st.button('Esegui l’analisi'):
-    tm_state = st.text('Modeling topics...')
-    text, dates, topic_model, topics = get_topic_model(lines)
-    tm_state.text('Modeling topics... done!')
+        lines = f.readlines()
+        def get_topic_model(lines):
+        topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
+        topics, probs = topic_model.fit_transform(lines)
+        freq = topic_model.get_topic_info(); freq.head(5)
+        return topics, freq, topic_model
     
-    with st.container():
+        def topic_model_visualize(topic_model):
+        return topic_model.visualize_topics()
+
+        def topic_model_distribution(topic_model):
+        return topic_model.visualize_distribution(probs[200], min_probability=0.015)
+
+        def topic_model_hierarchy(topic_model):
+        return topic_model.visualize_hierarchy(top_n_topics=50)
+
+        def topic_model_barchart(topic_model):
+        return topic_model.visualize_barchart(top_n_topics=5)
+        
+        if st.button('Esegui l’analisi'):
+        tm_state = st.text('Modeling topics...')
+        text, dates, topic_model, topics = get_topic_model(lines)
+        tm_state.text('Modeling topics... done!')
+    
+        with st.container():
         st.write("This is inside the container")
 
         fig1 = topic_model_visualize(topic_model)
