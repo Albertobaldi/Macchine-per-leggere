b0dc1ee9398448f94147180a8a2a7ea08ef0166f
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-31 14:05:20 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -38,6 +38,25 @@ st.title("BERTopic – Topic modeling e analisi dei temi su un corpus testuale")
 
 from nltk.corpus import stopwords
 stopwords = stopwords.words('italian')
+
+def get_topic_model():
+    text = lines
+    topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
+    topics, probs = topic_model.fit_transform(lines)
+    freq = topic_model.get_topic_info(); freq.head(5)
+    return topics, freq, topic_model
+    
+def topic_model_visualize(topic_model):
+    return topic_model.visualize_topics()
+
+def topic_model_distribution(topic_model):
+    return topic_model.visualize_distribution(probs[200], min_probability=0.015)
+
+def topic_model_hierarchy(topic_model):
+    return topic_model.visualize_hierarchy(top_n_topics=50)
+
+def topic_model_barchart(topic_model):
+    return topic_model.visualize_barchart(top_n_topics=5)
             
 uploaded_file = st.sidebar.file_uploader("Scegli un file di testo")
 st.sidebar.caption('Verifica che il file sia privo di formattazione')
@@ -45,24 +64,6 @@ st.sidebar.markdown("""---""")
 if uploaded_file is not None:
     with open(uploaded_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-       
-def topic_model():
-    topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
-    topics, probs = topic_model.fit_transform(lines)
-    freq = topic_model.get_topic_info(); freq.head(5)
-    return topics, freq, topic_model
-    
-def topic_model_visualize():
-    return topic_model.visualize_topics()
-
-def topic_model_distribution():
-    return topic_model.visualize_distribution(probs[200], min_probability=0.015)
-
-def topic_model_hierarchy():
-    return topic_model.visualize_hierarchy(top_n_topics=50)
-
-def topic_model_barchart():
-    return topic_model.visualize_barchart(top_n_topics=5)
     
 
 fig1 = topic_model_visualize()
