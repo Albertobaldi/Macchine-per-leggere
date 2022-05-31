8d6dd78e2d4461bb83cb752532fd3dbebef77cd6
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-05-31 15:33:08 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -39,7 +39,7 @@ st.title("BERTopic – Topic modeling e analisi dei temi su un corpus testuale")
 from nltk.corpus import stopwords
 stopwords = stopwords.words('italian')
 
-def get_topic_model():
+def get_topic_model(file):
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
     topics, probs = topic_model.fit_transform(file)
     freq = topic_model.get_topic_info(); freq.head(5)
