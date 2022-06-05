96e2b79f2d806211a6c3abf9d96d84bdabe94f7d
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-05 19:20:42 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -56,5 +56,6 @@ if st.button('Processa i dati'):
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True)
     topics, probs = topic_model.fit_transform(file)
     freq = topic_model.get_topic_info(); freq.head(5)
+    topic_model.get_topic(0)
     st.write(topics)
     st.write(freq)
