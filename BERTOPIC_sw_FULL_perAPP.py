2bd212795c68534ffc8fb8d8e40a37b0813d5898
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-09 17:47:35 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -44,10 +44,9 @@ st.subheader("Topic modeling e analisi dei temi su un corpus testuale")
 from sklearn.feature_extraction.text import CountVectorizer
 
 final_stopwords = st.sidebar.text_input("Inserisci una lista di stopwords, separate da una virgola (es. \"parola1, parola2, parola3\")", "")
-if final_stopwords is not None:
-    final_stopwords_list = final_stopwords.split(', ')
-else:
+    final_stopwords_appen = final_stopwords.split(', ')
     final_stopwords_list = stopwords.words('italian')
+    final_stopwords_list.append(final_stopwords_appen)
     
 vectorizer_model = CountVectorizer(stop_words = final_stopwords_list)
 
