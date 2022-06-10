a6485881d646f1d7077bea6e965c8d925d0a1ded
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-10 12:25:50 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -76,5 +76,7 @@ if st.button('Processa i dati'):
     st.plotly_chart(top, use_container_width=True)
     st.plotly_chart(distribution, use_container_width=True)
     parola = st.text_input('Cerca un topic per una parola')
-    topic_model.find_topics(parola)
+    if parola is not None:
+    topics = topic_model.find_topics(parola)
+    st.write(topics)
   
