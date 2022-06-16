a05e94e5a268b100631760d3be3840039175e8b9
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-16 20:20:54 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -56,8 +56,7 @@ st.sidebar.markdown("""---""")
 if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     file = stringio.read().split('\n')
-@st.cache
-if st.button('Processa i dati'):
+    if st.button('Processa i dati'):
     st.write("Il vostro file è in elaborazione. Il tempo impiegato nell’analisi dei topic può variare a seconda delle dimensioni del file di testo.")
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True, vectorizer_model=vectorizer_model)
     topics, probs = topic_model.fit_transform(file)
@@ -71,7 +70,6 @@ if st.button('Processa i dati'):
     st.plotly_chart(distribution, use_container_width=True)
     st.plotly_chart(heatmap, use_container_width=True)
     parola = st.text_input('Cerca un topic in base a una parola')
-    
     if parola is not None:
         topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True, vectorizer_model=vectorizer_model)
         topics, probs = topic_model.fit_transform(file)
