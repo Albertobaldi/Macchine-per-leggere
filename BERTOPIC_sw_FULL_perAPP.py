ffc985e7b840086dff36c72574fc6921587398a9
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-17 17:42:57 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -50,7 +50,7 @@ uploaded_file = st.sidebar.file_uploader("Scegli un file di testo")
 st.sidebar.caption('Verifica che il file sia privo di formattazione. Si raccomanda di convertire ogni fine di paragrafo in interruzione di linea (\\n): così facendo, l’algoritmo potrà suddividere il testo in paragrafi')
 st.sidebar.markdown("""---""")
 if uploaded_file is not None:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
 file = stringio.read().split('\n')
 
 if st.button('Processa i dati per visualizzare la distribuzione di tutti i topic'):
