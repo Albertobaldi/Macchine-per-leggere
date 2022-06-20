cfa14b6c74fdcf81709d0e9480aa1467d6abf868
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-20 17:29:14 +0200

    Update Prova_seconda.py

--- Prova_seconda.py
+++ Prova_seconda.py
@@ -65,6 +65,7 @@ if uploaded_file is not None:
 	
 if st.button('Processa i dati per visualizzare la distribuzione dei topic nel corpus'):
     st.write("Il vostro file è in elaborazione. Il tempo impiegato nell’analisi dei topic può variare a seconda delle dimensioni del file di testo.")
+    global topic_model
     topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True, vectorizer_model=vectorizer_model)
     topics, probs = topic_model.fit_transform(file)
     freq = topic_model.get_topic_info(); freq.head(10)
@@ -76,7 +77,6 @@ if st.button('Processa i dati per visualizzare la distribuzione dei topic nel co
     st.plotly_chart(top, use_container_width=True)
     st.plotly_chart(distribution, use_container_width=True)
     st.plotly_chart(heatmap, use_container_width=True)
-global topic_model
 	
 parola = st.text_input('Cerca un topic per una parola')
 if parola is not None:
