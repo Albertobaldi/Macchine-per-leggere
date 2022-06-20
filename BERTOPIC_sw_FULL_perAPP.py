f97c5c615d05569b774bd8cd2b3aaf724a7799c9
Albertobaldi <39796943+Albertobaldi@users.noreply.github.com>
2022-06-20 17:52:21 +0200

    Update BERTOPIC_sw_FULL_perAPP.py

--- BERTOPIC_sw_FULL_perAPP.py
+++ BERTOPIC_sw_FULL_perAPP.py
@@ -52,8 +52,8 @@ st.sidebar.markdown("""---""")
 if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     file = stringio.read().split('\n')
-	if file is not None:
-    		st.button('Processa i dati per visualizzare la distribuzione dei topic nel corpus')
+    if file is not None:
+	st.button('Processa i dati per visualizzare la distribuzione dei topic nel corpus')
 	if st.button is not None:
     		st.write("Il vostro file è in elaborazione. Il tempo impiegato nell’analisi dei topic può variare a seconda delle dimensioni del file di testo.")
    	 	topic_model = BERTopic(language="multilingual", calculate_probabilities=True, verbose=True, vectorizer_model=vectorizer_model)
